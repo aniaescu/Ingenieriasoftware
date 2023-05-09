52a2587b57b6a5de1b947a6cc648ae19681b47ae
UArevalo <103272747+UArevalo@users.noreply.github.com>
2023-05-09 16:08:48 +0200

    Delete __init__.py

--- __init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template
-
-app = Flask(__name__, template_folder='../templates')
-
-@app.route("/")
-@app.route("/<name>")
-def hello_world(name=None):
-    return render_template('hello.html', name=name)
