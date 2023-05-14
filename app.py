87df7dc130c5e882b3dd1c11662adba8d4c0fb30
Industria 4.0 <unai.chasco@opendeusto.es>
2023-05-14 10:32:11 +0200

    Login Google, herencia HTML y botón atrás

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
-from flask import Flask, render_template, request, redirect
+import os
+import pathlib
+import google
+import requests
+from flask import Flask, render_template, request, redirect, abort, session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.ext.mutable import MutableDict
 from sqlalchemy.sql import text
 from datetime import datetime
+from pip._vendor import cachecontrol
+from google.oauth2 import id_token
+from google_auth_oauthlib.flow import Flow
+
 
 app = Flask(__name__, template_folder='./templates')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bocaware.db'
@@ -39,8 +47,8 @@ class Pedidos (db.Model):
         return self.id
 
 @app.route("/")
-def hello_world(name=None):
-    return render_template('hello.html', name=name)
+def index():
+    return render_template('login.html')
 
 @app.route("/ingredientes", methods=['POST', 'GET'])
 def ingredientes():
@@ -139,3 +147,68 @@ def pedido_delete(id):
         return redirect('/pedidos')
     except:
         return("Error eliminando pedido")
+    
+
+
+
+app.secret_key = os.urandom(12)
+
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+GOOGLE_CLIENT_ID = "143267961120-okqij524o3ku5vojpu9baua7tmiua0mu.apps.googleusercontent.com"
+client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
+
+flow = Flow.from_client_secrets_file(
+    client_secrets_file=client_secrets_file,
+    scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
+    redirect_uri="http://127.0.0.1:5000/callback"
+)
+
+def login_is_required(function):
+    def wrapper(*args, **kwargs):
+        if "google_id" not in session:
+            return abort(401) 
+        else:
+            return function()
+    return wrapper
+
+
+@app.route("/login")
+def login():
+    authorization_url, state = flow.authorization_url()
+    session["state"] = state
+    return redirect(authorization_url)
+
+
+@app.route("/callback")
+def callback():
+    flow.fetch_token(authorization_response=request.url)
+
+    if not session["state"] == request.args["state"]:
+        abort(500)  
+
+    credentials = flow.credentials
+    request_session = requests.session()
+    cached_session = cachecontrol.CacheControl(request_session)
+    token_request = google.auth.transport.requests.Request(session=cached_session)
+
+    id_info = id_token.verify_oauth2_token(
+        id_token=credentials._id_token,
+        request=token_request,
+        audience=GOOGLE_CLIENT_ID
+    )
+
+    session["google_id"] = id_info.get("sub")
+    session["name"] = id_info.get("name")
+    return redirect("/index")
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
+
+@app.route("/index")
+@login_is_required
+def index():
+    return render_template("index.html")
